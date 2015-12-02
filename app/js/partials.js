cf45b7bf62ab9d415229413b1efdb575ae41b251
Edward Gaba <egaba@myfitnesspal.com>
2015-12-01 16:57:24 -0800

    setup various scenarios

--- app/js/partials.js
+++ app/js/partials.js
@@ -1,3 +1,21 @@
-Handlebars.registerPartial("square2", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div class=\"square-2\">\n\n</div>\n";
+Handlebars.registerPartial("solution-1", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"container\">\n  <div class=\"flex\">\n    <div class=\"flex-1\">\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"      ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "    </div>\n    <div>\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"      ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "    </div>\n  </div>\n</div>\n";
+},"usePartial":true,"useData":true}));
+Handlebars.registerPartial("solution-2", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true}));
+Handlebars.registerPartial("solution-3", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true}));
+Handlebars.registerPartial("solution-4", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true}));
+Handlebars.registerPartial("square", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<div class=\"square\"></div>\n";
 },"useData":true}));
--- app/js/templates.js
+++ app/js/templates.js
@@ -1,8 +1,30 @@
 this["MFP"] = this["MFP"] || {};
 this["MFP"]["templates"] = this["MFP"]["templates"] || {};
 this["MFP"]["templates"]["scenario-1"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div class=\"scenario-1\">\n  <h1>Scenario 1</h1>\n  <p>Create a simple header</p>\n\n</div>\n";
-},"useData":true});
+    var stack1;
+
+  return "<div class=\"scenario-1\">\n  <h1>Scenario 1</h1>\n  <p>Create a simple header</p>\n\n  <h2>Example</h2>\n"
+    + ((stack1 = this.invokePartial(partials['solution-1'],depth0,{"name":"solution-1","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "\n  <h2>Result</h2>\n</div>\n";
+},"usePartial":true,"useData":true});
 this["MFP"]["templates"]["scenario-2"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div class=\"scenario-2\">\n  <h1>Scenario 2</h1>\n  <p>Create a list</p>\n\n</div>\n";
-},"useData":true});
+    var stack1;
+
+  return "<div class=\"scenario-2\">\n  <h1>Scenario 2</h1>\n  <p>Center the box</p>\n\n  <h2>Example</h2>\n"
+    + ((stack1 = this.invokePartial(partials['solution-2'],depth0,{"name":"solution-2","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "\n  <h2>Result</h2>\n</div>\n";
+},"usePartial":true,"useData":true});
+this["MFP"]["templates"]["scenario-3"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"scenario-3\">\n  <h1>Scenario 3</h1>\n  <p>Create a list</p>\n\n  <h2>Example</h2>\n"
+    + ((stack1 = this.invokePartial(partials['solution-3'],depth0,{"name":"solution-3","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "\n  <h2>Result</h2>\n</div>\n";
+},"usePartial":true,"useData":true});
+this["MFP"]["templates"]["scenario-4"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"scenario-4\">\n  <h1>Scenario 4</h1>\n  <p>Re-order the content only using CSS</p>\n\n  <h2>Example</h2>\n"
+    + ((stack1 = this.invokePartial(partials['solution-4'],depth0,{"name":"solution-4","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "\n  <h2>Result</h2>\n</div>\n";
+},"usePartial":true,"useData":true});
