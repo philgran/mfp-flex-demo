0e67baa95eb218875b2361704b78f7fb03c5b4c2
Edward Gaba <egaba@myfitnesspal.com>
2015-12-03 13:57:36 -0800

    blah

--- app/js/partials.js
+++ app/js/partials.js
@@ -1,20 +1,37 @@
+Handlebars.registerPartial("logo", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<div class=\"logo\"></div>\n";
+},"useData":true}));
+Handlebars.registerPartial("lorem-long", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<p>\n  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet vehicula neque. Ut nec augue quis tellus sollicitudin mollis et vel quam. Nullam vel pulvinar nibh. Sed ornare tellus vel urna porta, at viverra elit iaculis. In nisl ipsum, maximus et risus a, suscipit tincidunt justo. Ut metus nisl, euismod vel volutpat nec, dignissim vitae ante. Sed in elit vestibulum, sagittis velit quis, consectetur odio.\n</p>\n\n<p>\n  Nulla non sem ornare, congue dolor non, imperdiet mi. Phasellus laoreet tincidunt erat sed congue. Donec eu ligula sed felis mollis varius vitae quis ipsum. Nulla odio sapien, pellentesque sit amet massa sit amet, suscipit mollis odio. In tellus nisi, porta aliquam tempus sit amet, aliquet vitae odio. Sed vulputate risus vitae lorem rhoncus, ac finibus purus pulvinar. Nulla congue auctor nisi at imperdiet.\n</p>\n\n<p>\n  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet vehicula neque. Ut nec augue quis tellus sollicitudin mollis et vel quam. Nullam vel pulvinar nibh. Sed ornare tellus vel urna porta, at viverra elit iaculis. In nisl ipsum, maximus et risus a, suscipit tincidunt justo. Ut metus nisl, euismod vel volutpat nec, dignissim vitae ante. Sed in elit vestibulum, sagittis velit quis, consectetur odio.\n</p>\n\n<p>\n  Nulla non sem ornare, congue dolor non, imperdiet mi. Phasellus laoreet tincidunt erat sed congue. Donec eu ligula sed felis mollis varius vitae quis ipsum. Nulla odio sapien, pellentesque sit amet massa sit amet, suscipit mollis odio. In tellus nisi, porta aliquam tempus sit amet, aliquet vitae odio. Sed vulputate risus vitae lorem rhoncus, ac finibus purus pulvinar. Nulla congue auctor nisi at imperdiet.\n</p>\n";
+},"useData":true}));
+Handlebars.registerPartial("lorem-short", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<p>\n  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet vehicula neque. Ut nec augue quis tellus sollicitudin mollis et vel quam. \n</p>\n";
+},"useData":true}));
+Handlebars.registerPartial("lorem", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<p>\n  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet vehicula neque. Ut nec augue quis tellus sollicitudin mollis et vel quam. Nullam vel pulvinar nibh. Sed ornare tellus vel urna porta, at viverra elit iaculis. In nisl ipsum, maximus et risus a, suscipit tincidunt justo. Ut metus nisl, euismod vel volutpat nec, dignissim vitae ante. Sed in elit vestibulum, sagittis velit quis, consectetur odio.\n</p>\n\n<p>\n  Nulla non sem ornare, congue dolor non, imperdiet mi. Phasellus laoreet tincidunt erat sed congue. Donec eu ligula sed felis mollis varius vitae quis ipsum. Nulla odio sapien, pellentesque sit amet massa sit amet, suscipit mollis odio. In tellus nisi, porta aliquam tempus sit amet, aliquet vitae odio. Sed vulputate risus vitae lorem rhoncus, ac finibus purus pulvinar. Nulla congue auctor nisi at imperdiet.\n</p>\n";
+},"useData":true}));
 Handlebars.registerPartial("solution-1", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"container\">\n  <div class=\"flex\">\n    <div class=\"flex-1\">\n"
-    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"      ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "    </div>\n    <div>\n"
-    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"      ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "    </div>\n  </div>\n</div>\n";
+  return "<div class=\"flex-container solution-1\">\n  <div class=\"flex flex-center media\">\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"    ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "  </div>\n  <div class=\"flex-1\">\n"
+    + ((stack1 = this.invokePartial(partials.lorem,depth0,{"name":"lorem","data":data,"indent":"    ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "  </div>\n</div>\n";
 },"usePartial":true,"useData":true}));
 Handlebars.registerPartial("solution-2", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
-},"useData":true}));
+    var stack1;
+
+  return "<div class=\"flex-container solution-2\">\n  <div class=\"flex-1\">\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"    ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "  </div>\n  <div class=\"flex-2\">\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"    ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "  </div>\n  <div class=\"flex-1\">\n"
+    + ((stack1 = this.invokePartial(partials.square,depth0,{"name":"square","data":data,"indent":"    ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
+    + "  </div>\n</div>\n";
+},"usePartial":true,"useData":true}));
 Handlebars.registerPartial("solution-3", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
-},"useData":true}));
-Handlebars.registerPartial("solution-4", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+    return "<div class=\"flex-container solution-3 text-center\">\n  <div class=\"flex-1 item-two\">\n    <p>\n      two\n    </p>\n  </div>\n  <div class=\"flex-1 item-three\">\n    <p>\n      three\n    </p>\n  </div>\n  <div class=\"flex-1 item-one\">\n    <p>\n      one\n    </p>\n  </div>\n</div>\n";
 },"useData":true}));
 Handlebars.registerPartial("square", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<div class=\"square\"></div>\n";
--- app/js/templates.js
+++ app/js/templates.js
@@ -3,28 +3,21 @@ this["MFP"]["templates"] = this["MFP"]["templates"] || {};
 this["MFP"]["templates"]["scenario-1"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"scenario-1\">\n  <h1>Scenario 1</h1>\n  <p>Create a simple header</p>\n\n  <h2>Example</h2>\n"
+  return "<div class=\"scenario-1\">\n  <h1>Scenario 1</h1>\n  <p>Create a \"media object\" whose media is centered</p>\n\n  <h2>Example</h2>\n"
     + ((stack1 = this.invokePartial(partials['solution-1'],depth0,{"name":"solution-1","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
     + "\n  <h2>Result</h2>\n</div>\n";
 },"usePartial":true,"useData":true});
 this["MFP"]["templates"]["scenario-2"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"scenario-2\">\n  <h1>Scenario 2</h1>\n  <p>Center the box</p>\n\n  <h2>Example</h2>\n"
+  return "<div class=\"scenario-2\">\n  <h1>Scenario 2</h1>\n  <p>Part 1: Create a 1-2-1 (4 column) layout. Center the content.</p>\n  <p>\n    Part 2: vertically stack your content.\n  </p>\n\n  <h2>Example</h2>\n"
     + ((stack1 = this.invokePartial(partials['solution-2'],depth0,{"name":"solution-2","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
     + "\n  <h2>Result</h2>\n</div>\n";
 },"usePartial":true,"useData":true});
 this["MFP"]["templates"]["scenario-3"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"scenario-3\">\n  <h1>Scenario 3</h1>\n  <p>Create a list</p>\n\n  <h2>Example</h2>\n"
+  return "<div class=\"scenario-3\">\n  <h1>Scenario 3</h1>\n  <p>Re-order the items without touching the HTML.</p>\n\n  <h2>Example</h2>\n"
     + ((stack1 = this.invokePartial(partials['solution-3'],depth0,{"name":"solution-3","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "\n  <h2>Result</h2>\n</div>\n";
-},"usePartial":true,"useData":true});
-this["MFP"]["templates"]["scenario-4"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var stack1;
-
-  return "<div class=\"scenario-4\">\n  <h1>Scenario 4</h1>\n  <p>Re-order the content only using CSS</p>\n\n  <h2>Example</h2>\n"
-    + ((stack1 = this.invokePartial(partials['solution-4'],depth0,{"name":"solution-4","data":data,"indent":"  ","helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "\n  <h2>Result</h2>\n</div>\n";
+    + "\n  <h2>Result</h2>\n  <div class=\"flex-container text-center\">\n    <div class=\"flex-1 item-two\">\n      <p>\n        two\n      </p>\n    </div>\n    <div class=\"flex-1 item-three\">\n      <p>\n        three\n      </p>\n    </div>\n    <div class=\"flex-1 item-one\">\n      <p>\n        one\n      </p>\n    </div>\n  </div>\n</div>\n";
 },"usePartial":true,"useData":true});
